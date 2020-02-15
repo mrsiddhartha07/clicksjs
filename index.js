ddadb6a89f9a116450437313c3a060700b9f33c7
rajat <rkumar201@sapient.com>
2020-02-15 15:48:33 +0530

    changes done for making test cases work

--- index.js
+++ index.js
@@ -1,81 +1,99 @@
 'use strict';
 
 const fs = require('fs');
+const { filePathName, outputPathName } = require('./constants');
 
-var sortIP = function(arg1, arg2) {
-    const ip1 = Number(arg1.ip.split(".").map((ip) => (`000${ip}`).slice(-3) ).join(""));
-    const ip2 = Number(arg2.ip.split(".").map((ip) => (`000${ip}`).slice(-3) ).join(""));
-    return ip1-ip2;
-  }
-  
-var sortTimeStamp = function(arg1, arg2) { 
-    let date1, date2;
-    date1 = new Date(arg1.timestamp);
-    date2 = new Date(arg2.timestamp)
-    return (date1.getTime() - date2.getTime())
-  }
-  
-Array.prototype.sortTime = function() {
-    var tempArr = [];
-    for(var i = 0; i < this.length - 1; i++){
-      if(this[i].ip !== this[i+1].ip){
-       let ob = {};
-        ob[this[i].ip] = this.splice(0, i + 1).sort(sortTimeStamp);
-        tempArr.push(ob);
-        i = -1;
-      }
+var sortIP = function (arg1, arg2) {
+  const ip1 = Number(arg1.ip.split(".").map((ip) => (`000${ip}`).slice(-3)).join(""));
+  const ip2 = Number(arg2.ip.split(".").map((ip) => (`000${ip}`).slice(-3)).join(""));
+  return ip1 - ip2;
+}
+
+var sortTimeStamp = function (arg1, arg2) {
+  let date1, date2;
+  date1 = new Date(arg1.timestamp);
+  date2 = new Date(arg2.timestamp)
+  return (date1.getTime() - date2.getTime())
+}
+
+Array.prototype.sortTime = function () {
+  var tempArr = [];
+  for (var i = 0; i < this.length - 1; i++) {
+    if (this[i].ip !== this[i + 1].ip) {
+      let ob = {};
+      ob[this[i].ip] = this.splice(0, i + 1).sort(sortTimeStamp);
+      tempArr.push(ob);
+      i = -1;
     }
-      var lastVal = {};
-      lastVal[this[0].ip] = this.sort(sortTimeStamp)
-      tempArr.push(lastVal)
-    return tempArr;
   }
+  var lastVal = {};
+  lastVal[this[0].ip] = this.sort(sortTimeStamp)
+  tempArr.push(lastVal)
+  return tempArr;
+}
 
-var getHashedArr = function(arr) {
-    arr = arr.filter((subSet)=>{
-        if(subSet[Object.keys(subSet)[0]].length < 10) {
-          let currentAmount, currentTimeObj, finalClicks=[];
-          subSet[Object.keys(subSet)[0]].forEach((ipObj)=> {
-            let ipTimeObj = new Date(ipObj.timestamp);
-            if(!currentTimeObj) {
-              currentTimeObj = ipTimeObj;
-              currentAmount = ipObj.amount;
-              finalClicks.push(ipObj);
-            }
-            else if(currentTimeObj.getHours() === ipTimeObj.getHours()) {
-              if(currentAmount < ipObj.amount) {
-                currentTimeObj = ipTimeObj;
-                currentAmount = ipObj.amount;
-                finalClicks.pop();
-                finalClicks.push(ipObj);
-              }
-              else if(currentAmount === ipObj.amount && (currentTimeObj.getTime() > ipTimeObj.getTime())) {
-                  currentTimeObj = ipTimeObj;
-                  finalClicks.pop();
-                  finalClicks.push(ipObj);
-              }
-            }
-            else {
-                currentTimeObj = ipTimeObj;
-                currentAmount = ipObj.amount;
-                finalClicks.push(ipObj);
-            }
-          });
-          subSet[Object.keys(subSet)[0]] = finalClicks;
-          return finalClicks
+var getHashedArr = function (arr) {
+  arr = arr.filter((subSet) => {
+    if (subSet[Object.keys(subSet)[0]].length < 10) {
+      let currentAmount, currentTimeObj, finalClicks = [];
+      subSet[Object.keys(subSet)[0]].forEach((ipObj) => {
+        let ipTimeObj = new Date(ipObj.timestamp);
+        if (!currentTimeObj) {
+          currentTimeObj = ipTimeObj;
+          currentAmount = ipObj.amount;
+          finalClicks.push(ipObj);
         }
+        else if (currentTimeObj.getHours() === ipTimeObj.getHours()) {
+          if (currentAmount < ipObj.amount) {
+            currentTimeObj = ipTimeObj;
+            currentAmount = ipObj.amount;
+            finalClicks.pop();
+            finalClicks.push(ipObj);
+          }
+          else if (currentAmount === ipObj.amount && (currentTimeObj.getTime() > ipTimeObj.getTime())) {
+            currentTimeObj = ipTimeObj;
+            finalClicks.pop();
+            finalClicks.push(ipObj);
+          }
+        }
+        else {
+          currentTimeObj = ipTimeObj;
+          currentAmount = ipObj.amount;
+          finalClicks.push(ipObj);
+        }
+      });
+      subSet[Object.keys(subSet)[0]] = finalClicks;
+      return finalClicks
+    }
+  });
+  return arr;
+}
+
+const results = function results(file, output) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf8', function (err, data) {
+      if (err) reject(err);
+      var input = JSON.parse(data);
+      var sortedArr = (input.sort(sortIP)).sortTime();
+      var newData = JSON.stringify(getHashedArr(sortedArr));
+      fs.writeFileSync(output, newData);
+      resolve(newData);
     });
-    return arr;
-  }
+  });
+}
 
-fs.readFile("clicks.json", function(err, data) {
-    if (err) throw err;
-    var input = JSON.parse(data);
-    var sortedArr = (input.sort(sortIP)).sortTime();
-    var newData = JSON.stringify(getHashedArr(sortedArr));
+results(filePathName, outputPathName).then(() => console.log("Please Find Result in resultset.json file"));
 
-    fs.writeFileSync("resultset.json", newData);
-    console.log("Please Find Result in result.json file");
+module.exports = results;
 
-});
+// fs.readFile("clicks.json", function (err, data) {
+//   if (err) throw err;
+//   var input = JSON.parse(data);
+//   var sortedArr = (input.sort(sortIP)).sortTime();
+//   var newData = JSON.stringify(getHashedArr(sortedArr));
+
+//   fs.writeFileSync("resultset.json", newData);
+//   console.log("Please Find Result in resultset.json file");
+
+// });
 
